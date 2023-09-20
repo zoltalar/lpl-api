32690d5ea4ba3c6723d4f3b67de29cd2e6708da6
zoltalar <voytechp@gmail.com>
2023-09-20 17:06:48 +0200

    users index action

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -6,9 +6,17 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\UserStoreRequest;
 use App\Http\Resources\BaseResource;
 use App\Models\User;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class UserController extends Controller
 {
+    public function index()
+    {
+        $users = QueryBuilder::for(User::class)->paginate();
+        
+        return BaseResource::collection($users);
+    }
+    
     public function store(UserStoreRequest $request)
     {
         $user = new User();
