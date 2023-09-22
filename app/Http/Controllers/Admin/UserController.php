f27e2ce8960e442a311a88c2a6f4f85173da01a9
zoltalar <voytechp@gmail.com>
2023-09-22 16:03:56 +0200

    search function for users

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -6,13 +6,19 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\UserStoreRequest;
 use App\Http\Resources\BaseResource;
 use App\Models\User;
+use Illuminate\Http\Request;
 use Spatie\QueryBuilder\QueryBuilder;
 
 class UserController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        $search = $request->search;
+        
         $users = QueryBuilder::for(User::class)
+            ->when($search, function($query) use ($search) {
+                return $query->search(['email'], $search);
+            })
             ->allowedSorts([
                 'users.id',
                 'users.email',
--- app/Models/Base.php
+++ app/Models/Base.php
@@ -2,9 +2,55 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 abstract class Base extends Model
 {
     public const DEFAULT_STRING_LENGTH = 255;
+    
+    // --------------------------------------------------
+    // Scopes
+    // --------------------------------------------------
+    
+    public function scopeSearch(Builder $query, array $columns, string $phrase = null, array $callbacks = []): Builder
+    {
+        if (empty($phrase)) {
+            $phrase = request()->get('search');
+        }
+
+        $phrase = (string) $phrase;
+
+        if (is_numeric($phrase) || filter_var($phrase, FILTER_VALIDATE_EMAIL)) {
+            $words = [$phrase];
+        } else {
+            $words = str_word_count($phrase, 1);
+        }
+
+        foreach ($words as $word) {
+            $word = trim($word);
+
+            foreach ($callbacks as $callback) {
+                if (function_exists($callback)) {
+                    $word = call_user_func($callback, $word);
+                }
+            }
+
+            $operator = 'like';
+
+            if (is_numeric($word)) {
+                $operator = '=';
+            } else {
+                $word = "%$word%";
+            }
+
+            $query->where(function ($builder) use ($columns, $operator, $word) {
+                foreach ($columns as $column) {
+                    $builder->orWhere($column, $operator, $word);
+                }
+            });
+        }
+
+        return $query;
+    }
 }
